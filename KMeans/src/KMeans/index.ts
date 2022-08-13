5ad5598e3a0f7e74dee527152081518fbaee53f0
formegusto <formegusto@gmail.com>
2022-08-14 00:56:37 +0900

    kmeans init

---
+++ KMeans/src/KMeans/index.ts
@@ -0,0 +1,8 @@
+class KMeans {
+  datas: number[][];
+  constructor(datas: number[][]) {
+    this.datas = datas;
+  }
+}
+
+export default KMeans;
--- KMeans/src/index.ts
+++ KMeans/src/index.ts
@@ -1,1 +1,12 @@
-console.log("test");
+import _ from "lodash";
+import KMeans from "./KMeans";
+
+// 1. data setting
+const TESTSIZE = 50;
+const testArray = _.map(new Array(TESTSIZE), () => [
+  Math.floor(Math.random() * 100) + 1,
+  Math.floor(Math.random() * 100) + 1,
+]);
+
+// 2. kmeans init
+const kmeans = new KMeans(testArray);

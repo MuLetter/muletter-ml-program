4305872bcd4b43d4599dc11868161b94dd469e0e
formegusto <formegusto@gmail.com>
2022-08-14 02:55:48 +0900

    kmeans html interaction okay

--- KMeans/src/index.ts
+++ KMeans/src/index.ts
@@ -1,50 +1,75 @@
 import _ from "lodash";
 import KMeans from "./KMeans";
 
-// 1. data setting
-const TESTSIZE = 100;
-const testArray = _.map(new Array(TESTSIZE), () => [
-  Math.floor(Math.random() * 100) + 1,
-  Math.floor(Math.random() * 100) + 1,
-]);
+let kmeans: KMeans;
 
-// 2. kmeans init
-const kmeans = new KMeans(testArray);
+function init() {
+  // 1. data setting
+  const TESTSIZE = 100;
+  const testArray = _.map(new Array(TESTSIZE), () => [
+    Math.floor(Math.random() * 100) + 1,
+    Math.floor(Math.random() * 100) + 1,
+  ]);
 
-// 3. init centroids
-kmeans.setCentroids(1);
-console.log(kmeans.centroids);
+  // 2. kmeans init
+  kmeans = new KMeans(testArray);
+
+  // 3. init centroids
+  kmeans.setCentroids(1);
+  console.log(kmeans.centroids);
+}
+
+init();
 
 // 4. iteration pattern next
 // 1 round
 do {
-  kmeans.next();
+  kmeans!.next();
   // console.log(kmeans.labels);
-} while (!kmeans.done);
+} while (!kmeans!.done);
 
 // DOM Test
-const colors: string[] = [];
-const elSVG = document.getElementById("scatter");
-for (let label of _.uniq(kmeans.labels)) {
-  let color = Math.floor(Math.random() * 16777215).toString(16);
-  while (color in colors) {
-    color = Math.floor(Math.random() * 16777215).toString(16);
-    console.log(color in colors);
-  }
-  colors.push(color);
-
-  const members = _.filter(kmeans.datas, (d, i) => kmeans.labels![i] === label);
-
-  members.forEach((member) => {
-    const circle = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "circle"
-    );
-    circle.setAttribute("cx", member[0].toString());
-    circle.setAttribute("cy", member[1].toString());
-    circle.setAttribute("r", "2");
-    circle.setAttribute("fill", "#" + color);
-
-    elSVG?.appendChild(circle);
+function render() {
+  const elSVG = document.getElementById("scatter");
+  const circles = document.querySelectorAll("circle");
+  circles.forEach((circle) => {
+    circle.remove();
   });
+
+  for (let label of _.uniq(kmeans!.labels)) {
+    const members = _.filter(
+      kmeans!.datas,
+      (d, i) => kmeans.labels![i] === label
+    );
+
+    members.forEach((member) => {
+      const circle = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "circle"
+      );
+      circle.setAttribute("cx", member[0].toString());
+      circle.setAttribute("cy", member[1].toString());
+      circle.setAttribute("r", "2");
+      circle.setAttribute("fill", "#" + kmeans.colors[label]);
+
+      elSVG?.appendChild(circle);
+    });
+  }
 }
+
+render();
+
+const elInit = document.querySelector<HTMLButtonElement>(".init");
+elInit?.addEventListener("click", () => {
+  init();
+  render();
+});
+
+const elNext = document.querySelector<HTMLButtonElement>(".next");
+elNext?.addEventListener("click", () => {
+  if (kmeans.done) alert("이미 종료된 kmeans 입니다.");
+  else {
+    kmeans.next();
+    render();
+  }
+});

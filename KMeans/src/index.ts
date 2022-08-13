7db9980a51687218bb9dcc9dca767a4db7fb0ffb
formegusto <formegusto@gmail.com>
2022-08-14 02:31:32 +0900

    kmeans html test

--- KMeans/src/index.ts
+++ KMeans/src/index.ts
@@ -1,9 +1,8 @@
-import _, { min } from "lodash";
+import _ from "lodash";
 import KMeans from "./KMeans";
-import { euclideanDistance } from "./KMeans/utils";
 
 // 1. data setting
-const TESTSIZE = 50;
+const TESTSIZE = 100;
 const testArray = _.map(new Array(TESTSIZE), () => [
   Math.floor(Math.random() * 100) + 1,
   Math.floor(Math.random() * 100) + 1,
@@ -17,10 +16,35 @@ kmeans.setCentroids(1);
 console.log(kmeans.centroids);
 
 // 4. iteration pattern next
-
 // 1 round
-
 do {
   kmeans.next();
-  console.log(kmeans.labels);
+  // console.log(kmeans.labels);
 } while (!kmeans.done);
+
+// DOM Test
+const colors: string[] = [];
+const elSVG = document.getElementById("scatter");
+for (let label of _.uniq(kmeans.labels)) {
+  let color = Math.floor(Math.random() * 16777215).toString(16);
+  while (color in colors) {
+    color = Math.floor(Math.random() * 16777215).toString(16);
+    console.log(color in colors);
+  }
+  colors.push(color);
+
+  const members = _.filter(kmeans.datas, (d, i) => kmeans.labels![i] === label);
+
+  members.forEach((member) => {
+    const circle = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "circle"
+    );
+    circle.setAttribute("cx", member[0].toString());
+    circle.setAttribute("cy", member[1].toString());
+    circle.setAttribute("r", "2");
+    circle.setAttribute("fill", "#" + color);
+
+    elSVG?.appendChild(circle);
+  });
+}

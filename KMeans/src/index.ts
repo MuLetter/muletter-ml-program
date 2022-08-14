bee456d3cc0650b5efb67b9d77e568e515a119fa
formegusto <formegusto@gmail.com>
2022-08-14 12:22:17 +0900

    total of sum sqaured

--- KMeans/src/index.ts
+++ KMeans/src/index.ts
@@ -18,9 +18,19 @@ const testArray = _.map(new Array(TESTSIZE), () => [
 const kmeans = new KMeans(testArray);
 
 // 3. kmeans++ set centroids
-kmeans.setCentroids(1);
-let v1Separation = 0;
-console.log(kmeans.clusterSeparation, kmeans.centroids);
+// kmeans.setCentroids(1);
+// console.log(kmeans.clusterSeparation, kmeans.centroids);
 
 kmeans.setCentroids(2);
 console.log(kmeans.clusterSeparation, kmeans.centroids);
+
+// 4. tss
+const dataMean = _.map(_.unzip(kmeans.datas), (zipData) => _.mean(zipData));
+const dataMeanDistances = _.map(
+  kmeans.datas,
+  (data) => euclideanDistance(dataMean, data) ** 2
+);
+const dataMeanDistance = _.sum(dataMeanDistances);
+console.log(dataMeanDistance);
+
+// 5. wss

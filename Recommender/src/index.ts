a16e455bd6b894ad33264199ab2afd5cee664f5c
formegusto <formegusto@gmail.com>
2022-08-15 19:06:05 +0900

    recommender + kmeans, test okay

--- Recommender/src/index.ts
+++ Recommender/src/index.ts
@@ -1,6 +1,8 @@
 import "module-alias/register";
 import RecommenderBuilder from "@recommender/builder";
 import _ from "lodash";
+import MinMaxScaler from "@minmax-scaler";
+import KMeans from "@kmeans";
 
 const mailBoxId = "62f9a6221b1ae3a082abce38";
 const builder = new RecommenderBuilder();
@@ -13,8 +15,16 @@ const recommender = builder.get();
   await builder.step3();
   await builder.step4();
 
-  console.log(recommender.processIds[0]);
-  console.log(recommender.processDatas[0]);
+  const scaler = new MinMaxScaler(recommender.processDatas as number[][]);
+  const datas = scaler.fit().transfrom();
+
+  const kmeans = new KMeans(datas);
+  kmeans.setCentroids(2);
+
+  do {
+    kmeans.next();
+  } while (!kmeans.done);
+  console.log(kmeans.labels);
 
   recommender.close();
 })();
